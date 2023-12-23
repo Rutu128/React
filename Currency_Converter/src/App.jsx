9f354b77c6e3de32fb1fcad90bc43edbd76423fc
Rutu <rutubhimani124@gmail.com>
2023-12-23 18:48:33 +0530

    added

---
+++ Currency_Converter/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+    <>
+     <h1 className="text-3xl text-center text-white font-mono shadow-lg h-12 bg-gray-700 shadow-gray-700 ">Currncy App</h1>
+    </>
+  )
+}
+
+export default App
