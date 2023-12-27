b7b02302b827d51f194d7842eb4df46ac01843f0
Rutu <rutubhimani124@gmail.com>
2023-12-27 14:47:31 +0530

    RR Add

---
+++ reactrouter/src/Layout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/header/Header";
+import Footer from "./components/Footer/Footer";
+import { Outlet } from "react-router-dom";
+function Layout() {
+    return (
+        <>
+            <Header />
+            <Outlet />
+            <Footer />
+        </>
+    )
+}
+export default Layout
